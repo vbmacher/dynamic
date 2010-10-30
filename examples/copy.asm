708f224f2368c19ca810dac9ce18e37546cf7210
Peter Jakubčo <pjakubco@gmail.com>
2010-10-30 11:51:34 +0200

    * Updated directory structure * Added source codes of some examples

---
+++ examples/copy.asm
@@ -0,0 +1,28 @@
+; COPY(X,Y)
+; input:
+;   reg.1: X
+;   reg.2: Y
+;
+; output:
+;   reg.X: (reg.Y)
+;   reg.Y: (reg.Y)
+
+
+; load X,Y
+read 1
+read 2
+
+; load r.X, r.Y
+read *1
+read *2
+
+; copy process
+load *2
+store *1
+
+write 1
+write 2
+write *1
+write *2
+
+halt
---
+++ examples/reverse.asm
@@ -0,0 +1,25 @@
+; input : X0 - string X ended by zero, X∈{1,2,3,....}*
+; output: XR - reversed string X
+
+load=10
+store 2
+
+citaj:
+  read1
+  load 1
+  jz vypis
+  store *2
+  load 2
+  add=1
+  store2
+  jmp citaj
+vypis:
+  load 2
+  sub=1
+  store 2
+  sub =9
+  jz koniec
+  write *2
+  jmp vypis
+koniec:
+  halt
